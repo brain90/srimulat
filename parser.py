6bb485b6b8dda79424be3d13b75886de8c7b5d97
Gibransyah Fakhri <gibransyah@gmail.com>
2016-02-26 17:03:08 +0700

    line parser

--- parser.py
+++ parser.py
@@ -7,44 +7,46 @@ class WhatsappChatParser(object):
 
     def __init__(self, chatFile):
         self.chatFile = chatFile
-        self.counter = 0
 
     def process(self):
       chatFile = open(self.chatFile, 'r')
-      lineBuffer = ""
       cleanFile = open("cleanFile.txt","w")
-      chatFile.next() 
-      chatFile.next()
+
+      # Move pointer to 3rd line
+      #for i in xrange(23):
+      #  chatFile.next()
+
+      lineBuffer = ""
+      counter = 0
       for line in chatFile:
-        line = line.rstrip()
-        sys.exit()
+        line = line.strip().replace("\r","")
         if (self.isNewRecord(line)):
-          cleanFile.write(lineBuffer+"\n")
-          cleanFile.write(line)
+          cleanFile.write(self.lineCleaner(lineBuffer+"\n"+line))
+          #cleanFile.write(self.lineCleaner(line))
           lineBuffer = ""
         else:
           lineBuffer = lineBuffer + " "  + line
-        self.lineCleaner(lineBuffer)
-        print self.counter
-        self.counter = self.counter + 1
+        counter += 1
+        #print counter
       cleanFile.close()
       print "done"
                      
     def isNewRecord(self, line):
-        if (re.match('^(\d{1,2})[/.-](\d{1,2})[/.-](\d{4})',line.strip()) is not None):
+        if (re.match('^(\d{1,2})[/.-](\d{1,2})[/.-](\d{4})',line) is not None):
             return True
         else:
             return False
 
     def lineCleaner(self, line):
-        msg_date, sep, msg = line.partition("- ")
-        raw_date, sep, time = msg_date.partition(" ")
+        msg_date, sep, msg = line.partition(" - ")
         sender, sep, content = msg.partition(": ")
-        datetime_obj = parser.parse(msg_date)
+        print line
         re.sub(r'[^\x00-\x7F]+','', content)
-        return datetime_obj.strftime("%Y-%m-%d %H:%M") + "\t" + \
-               sender + "\t" + content.replace('\r','') 
-
+        return "INSERT INTO srimulat " + \
+                "VALUES " + \
+                "('" + parser.parse(msg_date).strftime("%Y-%m-%d %H:%M") + "','" + \
+                sender + \
+                "',$$" + content.replace('\r','') + "$$);";
 
 ChatParser = WhatsappChatParser('message.txt');
 ChatParser.process()
