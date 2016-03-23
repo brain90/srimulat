108641e33431ca44f90a9358ceb84fd8c8391514
Gibransyah Fakhri <gibransyah@gmail.com>
2016-03-24 00:13:36 +0700

    Content without stop words

--- sql/WordFrequencyInEveryPost.sql
+++ sql/WordFrequencyInEveryPost.sql
@@ -1,7 +1,7 @@
 -- Frekuensi kata di semua posting
 SELECT left(word,30) as word, count(*)
 FROM ( 
-  SELECT regexp_split_to_table(content, '\s') as word
+  SELECT regexp_split_to_table(content_clean, '\s') as word
   FROM srimulat
 ) t 
 GROUP BY word order by 2 desc;
